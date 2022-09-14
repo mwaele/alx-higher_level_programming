e575d4bb79ea97f3f3f8ee44e49f7dce63a04c9d
mwaele <emmanuelmwaele@gmail.com>
2022-09-14 07:37:20 -0700

    INitial commit to project 0x06

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square:
+    """Represents a square.
+    Private instance attribute: size
+    Instantiation with size (no type/value verification).
+    """
+
+    def __init__(self, size):
+        """INitializes the data."""
+        self._size = size
