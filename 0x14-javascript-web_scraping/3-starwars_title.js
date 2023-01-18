1bfda3950f0172d3ed3559891f00d5a54d3e1d37
mwaele <emmanuelmwaele@gmail.com>
2023-01-17 23:06:42 -0800

    Initial commit to project 0x14

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const num = process.argv[2];
+
+const url = (`https://swapi.co/api/films/:id`);
+
+request(url, function (err, response, data) {
+
+  if (err) {
+
+    console.log(err);
+
+  } else {
+
+    console.log(JSON.parse(data).title);
+
+  }
+
+});
